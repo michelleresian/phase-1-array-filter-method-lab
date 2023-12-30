00529804898a5ec236c36c7d21f8f0bf220033cd
Michelle Resian <you@example.com>
2023-12-30 14:26:31 +0300

    filter method

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+//Function to find drivers whose names match the provided string (case-insensitive)
+function findMatching(drivers, query){
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+}
+//Function to find drivers whose names begin with the provided letters (case- insenstive)
+function fuzzyMatch(drivers, query){
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+
+}
+//Function to find driver objects whose name property matches the provided string (case-insensitive)
+function matchName(drivers, query){
+    return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+
+}
